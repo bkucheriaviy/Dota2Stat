4bc1bf7fae8e32ae91d6b3942855ea0dd4a7dbb8
Bohdan <bohdan.kucheriavuy@gmail.com>
2017-10-11 16:30:07 +0200

    Fixed build, added basic logging and basic console lops

--- D2S.Service/DataService.cs
+++ D2S.Service/DataService.cs
@@ -8,14 +8,14 @@ namespace Dota2Stat
     public class DataService : IDataService
     {
         private const string NAS_PATH_BASE = @"//MyCloud";
-        private const string DOTA2_EXTRACTION_PATH= @"Projects/Shared/Dota2Stat";
+        private const string DOTA2_EXTRACTION_PATH = @"Projects/Shared/Dota2Stat";
         private const string ITEMS = @"items.dvf";
-        
+
         public async Task Start(CancellationToken ct)
         {
             while (ct.IsCancellationRequested)
             {
-                
+
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
         }
--- D2S.Service/Program.cs
+++ D2S.Service/Program.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using D2S.Core;
+using Microsoft.Extensions.Logging;
 
 namespace Dota2Stat
 {
     public class Program
     {
+        private static readonly ILogger Log = ApplicationLogging.CreateLogger<Program>();
+
+        static Program()
+        {
+            ApplicationLogging.LoggerFactory.AddConsole(false);
+        }
+
         static void Main(string[] args)
         {
             var cts = new CancellationTokenSource();
             var token = cts.Token;
-            Console.WriteLine("Starting services...");
+            Log.LogInformation("Starting services...");
             Task.Run(async () => { await new DataService().Start(token); });
-            
+            Log.LogInformation("Services started.");
+            Console.WriteLine("Type 'exit' to shutdown...");
+            while (Console.ReadLine().ToLowerInvariant() != "exit")
+            {
+                Console.WriteLine("Type 'exit' to shutdown...");
+            };
+            Console.WriteLine("Goodbye!");
         }
     }
 }
